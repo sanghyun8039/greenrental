b72c9c1f3874dd4fd37204e11a375b2fce13aa64
SangHyun Choi <chltkdgus10@naver.com>
2022-10-03 06:59:43 +0900

    20221003

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,84 +1,72 @@
 import { React, useState } from "react";
 import styled from "styled-components";
 import { DateRangePicker } from "react-date-range";
-import "react-date-range/dist/styles.css";
-import "react-date-range/dist/theme/default.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { Button, StyledEngineProvider } from "@mui/material";
 import PeopleIcon from "@mui/icons-material/People";
 import { useNavigate } from "react-router-dom";
-const Wrapper = styled.div`
-  position: absolute;
-  top: 35px;
-  left: 25%;
-  width: 100vw;
-  Button {
-  }
-`;
-const Header2 = styled.h2`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 559px;
-  padding: 10px;
-  background-color: white;
-  position: absolute;
-  top: 325px;
-`;
-
-const Input = styled.input`
-  width: 539px;
-  padding: 20px;
-  position: absolute;
-  left: 0;
-  height: 30px;
-  top: 395px;
-  border: none;
-  &:focus {
-    outline-style: none;
-  }
-`;
-const MyButton = styled(Button)`
-  position: absolute;
-  left: 0;
-  top: 455px;
-  background-color: #ff7779;
-  color: white;
-  text-transform: inherit !important;
-  width: 579px;
-  &:hover {
-    background-color: white;
-    color: #ff7779;
-  }
-`;
-
+import { ko } from "date-fns/esm/locale";
 function Search() {
-  const navigate = useNavigate();
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date("2022/02/08"));
+  const [endDate, setEndDate] = useState(new Date("2022/02/10"));
 
-  const selectionRange = {
-    startDate: startDate,
-    endDate: endDate,
-    key: "selection",
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
   };
-
-  function handleSelect(ranges) {
-    setStartDate(ranges.selection.startDate);
-    setEndDate(ranges.selection.endDate);
-  }
   return (
-    <Wrapper>
-      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
-      {/* <Header2>
-        Number of guests
-        <PeopleIcon />
-      </Header2>
-      <Input min={0} defaultValue={2} type="number" />
-      <StyledEngineProvider injectFirst>
-        <MyButton onClick={() => navigate("/search")}>Search AirBNB</MyButton>
-      </StyledEngineProvider> */}
-    </Wrapper>
+    <DateWrapper>
+      <StartDate>
+        <h3>이때부터</h3>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          locale={ko} // 한글로 변경
+          dateFormat="yyyy.MM.dd (eee)" // 시간 포맷 변경
+          selectsStart
+          showPopperArrow={false}
+          startDate={startDate}
+          endDate={endDate}
+        />
+      </StartDate>
+      <EndDate>
+        <h3>이까지</h3>
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          locale={ko} // 한글로 변경
+          dateFormat="yyyy.MM.dd (eee)" // 시간 포맷 변경
+          selectsEnd
+          showPopperArrow={false}
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+        />{" "}
+      </EndDate>
+    </DateWrapper>
   );
 }
 
 export default Search;
+const DateWrapper = styled.div`
+  display: flex;
+`;
+const StartDate = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 5px;
+  margin-right: 10px;
+`;
+const EndDate = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 5px;
+  margin-left: 10px;
+`;
+const CustomDatePicker = styled(DatePicker)``;
--- src/components/SearchResultForm.jsx
+++ src/components/SearchResultForm.jsx
@@ -1,7 +1,7 @@
 import { React, useState } from "react";
 import styled from "styled-components";
-import { Box } from "@mui/material";
-import { InputBase } from "@mui/material";
+import { Box, Button, StyledEngineProvider } from "@mui/material";
+import { TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
@@ -15,6 +15,8 @@ import Search from "./Search";
 function SearchResultForm() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [showSearch, setShowSearch] = useState(false);
+  const [device, setDevice] = useState("");
 
   const selectionRange = {
     startDate: startDate,
@@ -26,38 +28,51 @@ function SearchResultForm() {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
   }
+
+  function handleSearch() {}
+
+  function handleChange(event) {
+    setDevice(event.target.value);
+  }
   return (
     <Wrapper>
       <CustomBox>
-        <SelectWhere>
-          <h1>어디에서 빌리시나요?</h1>
-          <InputContainer>
-            <CustomInput></CustomInput>
+        <SelectSection id="SelectSection">
+          <SelectWhere>
+            <h1>어디에서 빌리시나요?</h1>
+            <InputContainer>
+              <CustomTextField></CustomTextField>
 
-            <SearchIcon />
-          </InputContainer>
-        </SelectWhere>
+              <SearchIcon />
+            </InputContainer>
+          </SelectWhere>
 
-        <SelectWhat>
-          <h1>무엇을 빌리시나요?</h1>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">기계</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              label="Device"
-            >
-              <MenuItem>트렉터</MenuItem>
-              <MenuItem>컴바인</MenuItem>
-              <MenuItem>이양기</MenuItem>
-            </Select>
-          </FormControl>
-        </SelectWhat>
+          <SelectWhat>
+            <h1>무엇을 빌리시나요?</h1>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">기계</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Device"
+                onChange={handleChange}
+                value={device}
+              >
+                <MenuItem value="tracker">트렉터</MenuItem>
+                <MenuItem value="combine">컴바인</MenuItem>
+                <MenuItem value="test">이양기</MenuItem>
+              </Select>
+            </FormControl>
+          </SelectWhat>
 
-        <SelectWhen>
-          <h1>언제까지 빌리시나요?</h1>
-          <Search />
-        </SelectWhen>
+          <SelectWhen>
+            <h1>언제까지 빌리시나요?</h1>
+            <Search />
+          </SelectWhen>
+        </SelectSection>
+        <ButtonSection>
+          <Button onClick={() => handleSearch()}> 검색 </Button>
+        </ButtonSection>
       </CustomBox>
     </Wrapper>
   );
@@ -72,6 +87,7 @@ const Wrapper = styled.div`
 `;
 const CustomBox = styled(Box)`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
   width: 85vw;
@@ -82,19 +98,57 @@ const CustomBox = styled(Box)`
   padding: 20px;
 `;
 
-const SelectWhere = styled.div``;
+const SelectWhere = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
 const InputContainer = styled.div`
-  border: 2px solid black;
   display: flex;
   flex: 1;
   align-items: center;
   max-width: fit-content;
   padding: 5px;
 `;
-const CustomInput = styled(InputBase)`
+const CustomTextField = styled(TextField)`
   border: none;
   padding: 5px;
 `;
-const SelectWhat = styled.div``;
-const CustomSelect = styled.div``;
-const SelectWhen = styled.div``;
+const SelectWhat = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SelectWhen = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SelectSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 7vh;
+  width: 80vw;
+`;
+const ButtonSection = styled.div`
+  width: 80vw;
+  height: 7vh;
+  display: flex;
+  align-items: center;
+  Button {
+    width: 100%;
+    height: 100%;
+    background-color: #63af5c;
+    font-size: 30px;
+    color: yellow;
+    &:hover {
+      background-color: #5b5b5b;
+      color: white;
+    }
+  }
+`;
