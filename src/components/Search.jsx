62a741669df80a6eab38f8675296f5704d799301
SangHyun Choi <chltkdgus10@naver.com>
2022-10-11 23:11:39 +0900

    2022/10/11

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,26 +1,18 @@
 import { React, useEffect, useState } from "react";
 import styled from "styled-components";
-import { DateRangePicker } from "react-date-range";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Button, StyledEngineProvider } from "@mui/material";
-import PeopleIcon from "@mui/icons-material/People";
-import { useNavigate } from "react-router-dom";
 import { ko } from "date-fns/esm/locale";
 import { useDispatch } from "react-redux";
 import { setleftDay } from "../features/daySlice";
-function Search() {
-  const [startDate, setStartDate] = useState(new Date("2022/02/08"));
-  const [endDate, setEndDate] = useState(new Date("2022/02/10"));
+function Search(props) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
   const [diffDate, setDiffDate] = useState("");
-
   const dispatch = useDispatch();
-
-  const diffDays = () => {};
-
   const handleSelectEndDate = () => {
-    console.log(`startDate : ${startDate} // endDate : ${endDate}`);
-    setDiffDate((endDate - startDate) / (1000 * 60 * 60 * 24) + 1);
+    props.setPickerValue(false);
+    setDiffDate(Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24) + 1));
   };
 
   useEffect(() => {
@@ -29,48 +21,55 @@ function Search() {
         leftdays: diffDate,
       })
     );
-  }, [diffDate]);
-  // const onChange = (dates) => {
-  //   const [start, end] = dates;
-  //   setStartDate(start);
-  //   setEndDate(end);
-  // };
+  }, [diffDate, dispatch]);
+
   return (
     <DateWrapper>
-      <StartDate>
-        <h3>이때부터</h3>
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          locale={ko} // 한글로 변경
-          dateFormat="yyyy.MM.dd (eee)" // 시간 포맷 변경
-          selectsStart
-          showPopperArrow={false}
-          startDate={startDate}
-          endDate={endDate}
-        />
-      </StartDate>
-      <EndDate>
-        <h3>이까지</h3>
-        <DatePicker
-          selected={endDate}
-          onChange={(date) => setEndDate(date)}
-          locale={ko} // 한글로 변경
-          dateFormat="yyyy.MM.dd (eee)" // 시간 포맷 변경
-          selectsEnd
-          showPopperArrow={false}
-          startDate={startDate}
-          endDate={endDate}
-          minDate={startDate}
-          onCalendarClose={handleSelectEndDate}
-        />{" "}
-      </EndDate>
+      <DateLayout>
+        <StartDate>
+          <h3>이때부터</h3>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            locale={ko} // 한글로 변경
+            dateFormat="yyyy.MM.dd (eee)" // 시간 포맷 변경
+            selectsStart
+            showPopperArrow={false}
+            startDate={startDate}
+            endDate={endDate}
+          />
+        </StartDate>
+        <EndDate color={props.pickerErrorDisplay ? "#d32f2f" : "Black"}>
+          <h3>이까지</h3>
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            locale={ko} // 한글로 변경
+            dateFormat="yyyy.MM.dd (eee)" // 시간 포맷 변경
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            onCalendarClose={handleSelectEndDate}
+            placeholderText="Select Date"
+          />
+        </EndDate>
+      </DateLayout>
+
+      {props.pickerErrorDisplay && (
+        <ErrorMessage>{props.pickerHelperText}</ErrorMessage>
+      )}
     </DateWrapper>
   );
 }
 
 export default Search;
 const DateWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const DateLayout = styled.div`
   display: flex;
 `;
 const StartDate = styled.div`
@@ -84,8 +83,23 @@ const StartDate = styled.div`
 const EndDate = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid black;
+  border: 1px solid;
+  border-color: ${(props) => props.color};
   border-radius: 10px;
   padding: 5px;
   margin-left: 10px;
+`;
+
+const ErrorMessage = styled.p`
+  color: #d32f2f;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif !important;
+  font-weight: 400;
+  font-size: 0.75rem;
+  line-height: 1.66;
+  letter-spacing: 0.03333em;
+  text-align: left;
+  margin-top: 3px;
+  margin-right: 14px;
+  margin-bottom: 0;
+  margin-left: 14px;
 `;
